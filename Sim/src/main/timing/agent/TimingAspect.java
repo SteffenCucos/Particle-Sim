7ba54a7e59a175fad7628c4bb375c2b058dd77a3
Steffen Cucos <steffen.cucos@gmail.com>
2021-11-22 23:36:13 -0500

    Update timing aspect to use logger, handle exceptions

--- Sim/src/main/timing/agent/TimingAspect.java
+++ Sim/src/main/timing/agent/TimingAspect.java
@@ -1,5 +1,7 @@
 package main.timing.agent;
 
+import java.util.logging.Logger;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Around;
@@ -8,16 +10,31 @@ import main.simulator.Simulator.Time;
 
 @Aspect
 public class TimingAspect {
-
+	
+	private static final Logger LOGGER = Logger.getLogger(TimingAspect.class.getName());
+	
 	@Around("execution(@Time * *(..)) && @annotation(timeAnnotation)")
 	public Object time(ProceedingJoinPoint proceedingJoinPoint, Time timeAnnotation) throws Throwable {
 		long startTime = System.currentTimeMillis();
-
-		Object result = proceedingJoinPoint.proceed();
-
-		long endTime = System.currentTimeMillis();
-		System.out.println(timeAnnotation.value() + " took " + (endTime - startTime) + " milliseconds");
-
-		return result;
+		Exception exception = null;
+		try {
+			return proceedingJoinPoint.proceed();
+		} catch (Exception e) {
+			exception = e;
+			throw e; // Re-throw the exception, the caller needs to know about it.
+		} finally {
+			/* According to https://docs.oracle.com/javase/tutorial/essential/exceptions/finally.html
+			 * finally blocks always run immediately after the try/catch blocks finish, so stopping the 
+			 * stop watch here will be accurate.*/
+			long endTime = System.currentTimeMillis();
+			String timeTaken = (endTime - startTime) + " milliseconds";
+			String outcome = exception == null
+					? "took"
+					: String.format("failed with exception: {%s} after", exception.getMessage());
+			
+			String msg = String.format("%s %s %s", timeAnnotation.value(), outcome, timeTaken);
+			//System.out.println(msg);
+			LOGGER.info(msg);
+		}
 	}
 }
